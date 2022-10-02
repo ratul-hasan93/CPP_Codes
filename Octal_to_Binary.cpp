491fb5913b464a6e035298ea72f0633b1bf62e93
Ratul Hasan Rafi <83693046+ratul-hasan93@users.noreply.github.com>
2022-10-02 09:16:14 +0100

    Update Octal_to_Binary.cpp

--- Octal_to_Binary.cpp
+++ Octal_to_Binary.cpp
@@ -2,11 +2,11 @@
 #include<bits/sdtc++.h>
 using namespace std;
 int main(){
-int n,sum=0, bc=1;
+int n,sum=0, oc=1;
   cin>>n;
   for(int i=n; i!=0; i/=2){
-  sum+=(i%2)*bc;
-    bc*=8;
+  sum+=(i%2)*oc;
+    oc*=8;
   }
   cout<<sum<<endl;
   return 0;
