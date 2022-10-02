5379de8b8e71e8169dd182211bedb82382abd9d2
Ratul Hasan Rafi <83693046+ratul-hasan93@users.noreply.github.com>
2022-10-02 12:36:35 +0100

    Create Swaping.cpp

---
+++ Swaping.cpp
@@ -0,0 +1,11 @@
+// Swaping two value with temporary variable
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int a,b;
+  cin>>a>>b;
+  int t;
+  t=a;//t=5
+  a=b;//a=7
+  b=t;//b=5
+}
