a826301fcdee9b1db9707485e2af01ea3f5fae26
Ratul Hasan Rafi <83693046+ratul-hasan93@users.noreply.github.com>
2022-10-02 12:27:19 +0100

    Create Fahrenheigth_to_celcius.cpp

---
+++ Fahrenheigth_to_celcius.cpp
@@ -0,0 +1,11 @@
+//ahrenhiegth to celcius
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+double f;
+  cout<<"Enter your Input: ";
+  cin>>f;
+  f=((c*9)/5)+32;
+  cout<< fahrenheigth is: <<f;
+  return 0;
+}
