7953e8bfa97c4ca0990f2cdf0030fdf7a49bc7c8
Ratul Hasan Rafi <83693046+ratul-hasan93@users.noreply.github.com>
2022-10-02 09:13:40 +0100

    Create Octal_to_Binary.cpp

---
+++ Octal_to_Binary.cpp
@@ -0,0 +1,13 @@
+// Octal to Binary Problem
+#include<bits/sdtc++.h>
+using namespace std;
+int main(){
+int n,sum=0, bc=1;
+  cin>>n;
+  for(int i=n; i!=0; i/=2){
+  sum+=(i%2)*bc;
+    bc*=8;
+  }
+  cout<<sum<<endl;
+  return 0;
+}
